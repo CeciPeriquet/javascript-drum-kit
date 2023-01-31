3bdc42de9e9baba934497ba93dac4cc52520ad36
Cecilia Periquet <periwhat@gmail.com>
2023-01-31 15:16:51 +0100

    Update footer, add function to change key class while playing

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 'use strict';
 
+const keys = document.querySelectorAll('.key');
+
 function handleKeys(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
+  key.classList.add('playing');
 }
 
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', handleKeys);
